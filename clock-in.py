ba04fde3fce1845ac9bf8ed6d00cb4bef1368e04
zpqqq10 <75430435+zpqqq10@users.noreply.github.com>
2022-09-08 08:55:08 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -115,10 +115,10 @@ class ClockIn(object):
         new_info['number'] = number
         new_info["date"] = self.get_date()
         new_info["created"] = round(time.time())
-#         new_info["address"] = "浙江省杭州市西湖区"
-#         new_info["area"] = "浙江省 杭州市 西湖区"
-        new_info["address"] = "广东省东莞市寮步镇碧园路乐荟城"
-        new_info["area"] = "广东省 东莞市"
+        new_info["address"] = "浙江省杭州市西湖区"
+        new_info["area"] = "浙江省 杭州市 西湖区"
+#         new_info["address"] = "广东省东莞市寮步镇碧园路乐荟城"
+#         new_info["area"] = "广东省 东莞市"
         new_info["province"] = new_info["area"].split(' ')[0]
         new_info["city"] = new_info["area"].split(' ')[1]
         # form change
